86e34ad0ed7acd6b02d6ce23fafa29f8efe4c417
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-04 23:53:18 +0100

    navbar 2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import NavBar from './components/NavBar';
+import NavBar from './components/nav/NavBar';
 
 function App() {
   return (
    <div>
-    Hi maya
+    <NavBar/>
    </div>
    
   );
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../nav/NavBar.css"
 
 
 const navbar = () => {
