73d5c0228e551a61ddc643fc8f51d769b076f2d1
Areebah-snow <mudathiraisha657@gmail.com>
2024-01-04 23:09:33 +0100

    Basic looks

--- src/components/benefit/BenefitDtl.jsx
+++ src/components/benefit/BenefitDtl.jsx
@@ -1,16 +1,32 @@
-const BenefitDtl = () => {
+
+const BenefitDtl = ({detailsArray}) => {
+
     return (
         <div>
-           <div className="right-container">
+           
+            {detailsArray.map((data, index)=>(
+                
+             <div  key={index} className="right-container"> 
                 <div className="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                    </svg> </div>
-                    <div className="details text-light-brown-1">
-                        <h1 className="font-bold">Best Quality</h1>
-                        <p>Made exclusively using the premium quality materials available in the city</p>
-                    </div>
-            </div> 
+                <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5"
+                 stroke="currentColor" 
+                 class="w-6 h-6"
+                 dangerouslySetInnerHTML={{ __html: data.icon }}>
+                
+                 </svg>
+                </div>
+                <div className="details text-light-brown-1">
+                        <h1 className="font-bold">{data.heading}</h1>
+                        <p>{data.details}</p>
+                </div>
+                </div> 
+            ))}
+             
+            
         </div>
       );
 }
