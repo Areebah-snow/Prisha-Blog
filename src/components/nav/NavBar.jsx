56e27854da0bb864d12363a69f6c85d828b2d473
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-05 00:08:28 +0100

    navbar 2nd commit

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import "../nav/NavBar.css"
+import {magnifying-glass} from '@heroicons/react/outline';
 
 
 const navbar = () => {
   return (
-    <nav>
+    <nav className=''>
       {/* logo */}
      <div>
      <h1>Prisha</h1>
      </div>
       {/* nav menu */}
-     <div>
+     <div className=''>
       <ul>
         <li>Home</li>
         <li>About</li>
@@ -19,7 +20,7 @@ const navbar = () => {
       </ul>
      </div>
       {/* navn icons */}
-     <div>
+     <div className='relative'>
       <ul>
         
       </ul>
