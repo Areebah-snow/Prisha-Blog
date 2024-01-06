b0502ee24e3bd2c0c0286aa9479aadde9a798d35
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-06 20:32:00 +0100

    searchbar commit

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -21,14 +21,14 @@ const navbar = () => {
       </div>
       {/* navn icons */}
       <div className="search_bar">
-        <input type="text" placeholder="search"/>
+        <input type="text" placeholder="Furniture Sets"/>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="w-5 h-5"
           >
             <path
               stroke-linecap="round"
