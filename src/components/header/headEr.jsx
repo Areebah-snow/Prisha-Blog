56917c7937f7536b49ba160ac141650261c473c4
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-06 21:49:04 +0100

    header section

---
+++ src/components/header/headEr.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../header/HeadEr.css';
+import HeadImg from "./../../images/img2.jpg";
+
+const headEr = () => {
+  return (
+    <div className='head'>
+      <div className="head_txt"></div>
+      <div className="head_img">
+        <img className="header-image" src={HeadImg} />
+      </div>
+    </div>
+  )
+}
+
+export default headEr;
