ade04c1d811fd751360851cfe186769c9713e9bc
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-06 19:42:55 +0100

    navbar 3rd  commit

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
-import "../nav/NavBar.css"
-// import {magnifying-glass} from '@heroicons/react/outline';
-
+import React from "react";
+import "../nav/NavBar.css";
 
 const navbar = () => {
   return (
-    <nav className='navbar flex justify-between items-center w-[100%] z-30'>
+    <nav className="navbar flex justify-between items-center w-[100%] z-30">
       {/* logo */}
-     <div className='logo '>
-     <h1>Pri<span className='lg'>sha.</span></h1>
-     </div>
+      <div className="logo ">
+        <h1>
+          Pri<span className="lg">sha.</span>
+        </h1>
+      </div>
       {/* nav menu */}
-     <div className=''>
-      <ul className='nav-menu flex justify-between'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Shop</li>
-        <li>Cart</li>
-      </ul>
-     </div>
+      <div className="">
+        <ul className="nav-menu flex justify-between">
+          <li>Home</li>
+          <li>About</li>
+          <li>Shop</li>
+          <li>Cart</li>
+        </ul>
+      </div>
       {/* navn icons */}
-     <div className='nav-icon relative'>
-      <ul>
-        
-      </ul>
-     </div>
+      <div className="nav-icon relative">
+        <ul>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default navbar;
