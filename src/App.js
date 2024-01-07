e628feea4561a56400c1180787b8f17ca6dde2b8
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-07 23:32:29 +0100

    header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import NavBar from './components/nav/NavBar';
-import HeadEr from './components/header/HeadEr';
+// import HeadEr from './components/header/HeadEr';
 
 function App() {
   return (
    <div>
     <NavBar/>
-    <HeadEr/>
+    {/* <HeadEr/> */}
    </div>
    
   );
