9c71e4c84fb338cc0342595e32a8e326aed58826
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-07 23:21:58 +0100

    header section

--- src/components/header/headEr.jsx
+++ src/components/header/headEr.jsx
@@ -5,7 +5,9 @@ import HeadImg from "./../../images/img2.jpg";
 const headEr = () => {
   return (
     <div className='head'>
-      <div className="head_txt"></div>
+      <div className="head_txt">
+        <h1>We Unleash Home Asthetics</h1>
+      </div>
       <div className="head_img">
         <img className="header-image" src={HeadImg} alt='furniture1'/>
       </div>
