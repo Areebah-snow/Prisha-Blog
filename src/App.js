e4e47ce05c5486afa7e3ee4c6d667395afca31ea
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-08 16:13:40 +0100

    branch updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import NavBar from './components/nav/NavBar';
-// import HeadEr from './components/header/HeadEr';
 
 function App() {
   return (
    <div>
     <NavBar/>
-    {/* <HeadEr/> */}
    </div>
    
   );
