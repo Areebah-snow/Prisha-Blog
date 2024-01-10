a982df0c8d8127b6804b9b73585000e33330e164
Areebah-snow <mudathiraisha657@gmail.com>
2024-01-10 21:38:16 +0100

    started testimonials

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,14 @@
+const Testimonials = () => {
+    return ( 
+
+        <div>
+            <div className="main-container">
+                <h1 className="heading">Testimonials</h1>
+
+            </div>
+          
+        </div>
+     );
+}
+ 
+export default Testimonials;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Benefit from './components/benefit/Benefit';
+import Testimonials from './components/testimonials/Testimonials';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Benefit/>
+    <Testimonials/>
   </React.StrictMode>
 );
 
