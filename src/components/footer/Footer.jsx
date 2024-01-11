f995f81e3f2430fab5ef209b26075501144749ea
Areebah-snow <mudathiraisha657@gmail.com>
2024-01-11 22:48:39 +0100

    New commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+
+        <div>
+            <div className="footer">
+                <h1>Prisha</h1>
+            </div>
+        </div>
+      );
+}
+ 
+export default Footer;
