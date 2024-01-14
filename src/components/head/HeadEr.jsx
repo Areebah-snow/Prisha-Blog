754f43801a1ad8ca380c62b8345f624afac3b64f
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-14 01:57:21 +0100

    new adjustments

--- src/components/head/HeadEr.jsx
+++ src/components/head/HeadEr.jsx
@@ -3,12 +3,15 @@ import "../head/HeadEr.css";
 
 const HeadEr = () => {
   return (
-    <div className='pt-10 '>
+    // header
+    <div className='pt-10 hero-sec '>
        <div className="head flex flex-wrap items-center justify center mx-auto mt-10 md:px-12 md:flex-row">
+        {/* text section */}
         <div className="text-sec mb-14 lg:mb-10 lg:w-1/2">
             <h1 className="hd_txt max-w-xl text-[4rem] bold-xl leading-none mb-5">We create home more asthetic</h1>
             <p className="first_p text-[1.2rem] ">Enjoy the new furnitures every time <br /> you move and feel the castle house <br /> of your dreams</p>
         </div>
+        {/* image section */}
         <div className="img-sec">image</div>
        </div>
     </div>
