405028c9a99275f70c262ed3d9a1996d3591fbe3
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-14 01:30:18 +0100

    header text styling

--- src/components/head/HeadEr.jsx
+++ src/components/head/HeadEr.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ""
 
 const HeadEr = () => {
   return (
-    <div>
-      
+    <div className='py-5 '>
+       <div className="head flex flex-wrap items-center justify center mx-auto mt-10 md:px-12 md:flex-row">
+        <div className="text-sec mb-14 lg:mb-10"></div>
+        <div className="img-sec"></div>
+       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeadEr
+export default HeadEr;
