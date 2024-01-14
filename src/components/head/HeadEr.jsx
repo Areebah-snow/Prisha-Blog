1e91a53671c6a12ce86e84bcd297dd5a80955dc2
phyeen <mofiyinfoluwaoloyede5@gmail.com>
2024-01-14 23:53:51 +0100

    header image adjustment

--- src/components/head/HeadEr.jsx
+++ src/components/head/HeadEr.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../head/HeadEr.css";
-import HeroImg from "./../../images/img7.jpg";
+import HeroImg from "./../../images/img8.jpg";
 
 const HeadEr = () => {
   return (
@@ -9,7 +9,7 @@ const HeadEr = () => {
        <div className="head flex flex-wrap items-center justify center mx-auto mt-10 md:px-12 md:flex-row">
         {/* text section */}
         <div className="text-sec mb-14 lg:mb-10 lg:w-1/2">
-            <h1 className="hd_txt max-w-xl text-[4rem] bold-xl leading-none mb-5">We create home more asthetic</h1>
+            <h1 className="hd_txt max-w-xl text-[5rem] bold-xl leading-none mb-5">We create home more asthetic</h1>
             <p className="first_p text-[1.2rem] ">Enjoy the new furnitures every time <br /> you move and feel the castle house <br /> of your dreams</p>
         </div>
         {/* image section */}
