07f3a8fdf05ef72140eca83a3ddf175d1f96a631
Areebah-snow <mudathiraisha657@gmail.com>
2024-01-16 22:02:48 +0100

    starting footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,26 @@ const Footer = () => {
 
         <div>
             <div className="footer">
+              <div className="left">
                 <h1>Prisha</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Reiciendis architecto doloremque eum quo, sit corporis laborum tenetur
+                     laboriosam minima dignissimos.</p>
+              </div>
+              <div className="right">
+                <table>
+                    <tr>
+                        <th>About</th>
+                        <th>Categories</th>
+                        <th>Contact</th>
+                    </tr>
+                    <tr>
+                        <td>About Us</td>
+                        <td>Cart</td>
+                        <td>prisha1234@gmail.com</td>                        
+                    </tr>
+                </table>
+              </div>
             </div>
         </div>
       );
